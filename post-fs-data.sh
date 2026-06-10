28b86c8f5cd2a4b40de69c55b4037202d4213d79
Samiul Basir Fahim <samiulbasirfahim.rxen@gmail.com>
2026-06-10 17:19:18 +0600

    faet: add a max retry counter on post-fs-data.sh to avoid bootloop

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,9 +1,16 @@
 #!/system/bin/sh
 
-while [ ! -d /system/odm/lib64/camera ]; do
-  sleep 0.5
+TARGET=/system/odm/lib64/camera
+
+count=0
+while [ ! -d $TARGET ]; do
+    count=$((count + 1))
+    if [ $count -ge 5 ]; then
+        exit 1
+    fi
+    sleep 0.5
 done
 
-chcon -R u:object_r:same_process_hal_file:s0 /system/odm/lib64/camera/
+chcon -R u:object_r:same_process_hal_file:s0 $TARGET
 
-echo "camera selinux fix applied" > /dev/kmsg
+echo "camera selinux fix applied" >/dev/kmsg
