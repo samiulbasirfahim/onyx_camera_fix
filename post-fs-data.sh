99da18a5312a38b50a94eca415b8fe3442b1f7eb
Samiul Basir Fahim <samiulbasirfahim.rxen@gmail.com>
2026-06-10 17:14:09 +0600

    feat: removed restorecon since it revert back

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -5,4 +5,5 @@ while [ ! -d /system/odm/lib64/camera ]; do
 done
 
 chcon -R u:object_r:same_process_hal_file:s0 /system/odm/lib64/camera/
-restorecon -RF /system/odm/lib64/camera/
+
+echo "camera selinux fix applied" > /dev/kmsg
