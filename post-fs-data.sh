3f04aef6eeaba063c99d0fb8523ba6eb69b62a6b
Samiul Basir Fahim <samiulbasirfahim.rxen@gmail.com>
2026-06-10 17:05:58 +0600

    feat: post-fs-data script for fixing permission.

---
+++ post-fs-data.sh
@@ -0,0 +1,8 @@
+#!/system/bin/sh
+
+while [ ! -d /system/odm/lib64/camera ]; do
+  sleep 0.5
+done
+
+chcon -R u:object_r:same_process_hal_file:s0 /system/odm/lib64/camera/
+restorecon -RF /system/odm/lib64/camera/
